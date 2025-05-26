ce33bb9188f9333070048613a1f75817409e4e6f
Vitoria <0001065043@senaimgaluno.com.br>
2025-05-26 14:18:11 -0300

    zip?

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.UseDefaultFiles(); // Serve index.html por padrão
+app.UseStaticFiles(); // Permite servir arquivos da pasta wwwroot
+
+
+app.MapGet("/index", async context =>
+{
+await context.Response.SendFileAsync("wwwroot/index.html");
+});
 
 app.Run();
     
