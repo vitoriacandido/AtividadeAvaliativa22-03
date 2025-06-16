b2db9fd297e4834d6e57d5260f13893fe5c1ab83
Vitoria <0001065043@senaimgaluno.com.br>
2025-06-16 13:11:34 -0300

    comentarios

--- obj/Debug/net8.0/AtividadeAvaliativa22-03.AssemblyInfo.cs
+++ obj/Debug/net8.0/AtividadeAvaliativa22-03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtividadeAvaliativa22-03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96f7a64738a5a839259a1b75587efa83f8052d7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ef62eb7dee6ff94783ba3d8b587ec2ffbe742e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtividadeAvaliativa22-03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtividadeAvaliativa22-03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
